c2922b9127e4fa2773135d5b632075b966c4432e
Sharik Hassan <120386711+shark4real@users.noreply.github.com>
2025-08-04 19:34:39 +0530

    Update urls.py

--- generator/urls.py
+++ generator/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('home/', views.generate_email, name='home'),
     path('authorize/', views.authorize_gmail, name='authorize_gmail'),
     path('oauth2callback/', views.oauth2callback, name='oauth2callback'),
+    path('send-single/', views.send_single_email, name='send_single_email'),
     path('bulk-preview/', views.bulk_preview, name='bulk_preview'),
     path('send-bulk/', views.send_bulk_email, name='send_bulk'),
 ]
